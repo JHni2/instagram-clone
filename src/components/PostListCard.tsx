521a56653ea49ada1852531c4f15f5cd3744358d
Jihyun <wlgus0260@naver.com>
2023-07-18 17:13:51 +0900

    refactor: 포스트 목록 (리팩토링)

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,16 +1,15 @@
 import { SimplePost } from '@/model/post';
-import { parseDate } from '@/util/date';
 import Image from 'next/image';
+import ActionBar from './ActionBar';
 import Avatar from './Avatar';
-import BookmarkIcon from './ui/icons/BookmarkIcon';
-import CommentIcon from './ui/icons/CommentIcon';
-import HeartIcon from './ui/icons/HeartIcon';
+import CommentForm from './CommentForm';
 
 type Props = {
   post: SimplePost;
+  priority?: boolean;
 };
 
-export default function PostListCard({ post }: Props) {
+export default function PostListCard({ post, priority = false }: Props) {
   const { userImage, username, image, createdAt, likes, text } = post;
 
   return (
@@ -19,24 +18,9 @@ export default function PostListCard({ post }: Props) {
         <Avatar image={userImage} size="medium" highlight />
         <span className="text-gray-900 font-bold ml-2">{username}</span>
       </div>
-      <Image className="w-full object-cover aspect-square" src={image} alt={`photo by ${username}`} width={500} height={500} />
-      <div className="flex justify-between my-2 px-4">
-        <HeartIcon />
-        <BookmarkIcon />
-      </div>
-      <div className="px-4 py-1">
-        <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}</p>
-        <p>
-          <span className="font-bold mr-1">{username}</span>
-          {text}
-        </p>
-        <p className="text-xs text-neutral-500 uppercase my-2">{parseDate(createdAt)}</p>
-        <form className="flex items-center border-t border-neutral-300">
-          <CommentIcon />
-          <input className="w-full ml-2 border-none outline-none p-3" type="text" placeholder="Add a comment..." />
-          <button className="font-bold text-sky-500 ml-2">Post</button>
-        </form>
-      </div>
+      <Image className="w-full object-cover aspect-square" priority={priority} src={image} alt={`photo by ${username}`} width={500} height={500} />
+      <ActionBar likes={likes} username={username} text={text} createdAt={createdAt} />
+      <CommentForm />
     </article>
   );
 }
