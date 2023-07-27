07fc20787cfbf0cbed8ef216866d15a678983943
Jihyun <wlgus0260@naver.com>
2023-07-27 17:16:18 +0900

    feat: 사용자 검색 (컴포넌트)

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,13 +1,34 @@
 'use client';
 
-import { useState } from 'react';
+import { ProfileUser } from '@/model/user';
+import { FormEvent, useState } from 'react';
+import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('');
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
+  const { data: users, isLoading, error } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
 
-  console.log(data);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault;
+  };
 
-  return <div></div>;
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" autoFocus placeholder="Search for a username or name" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      </form>
+      {error && <p>무언가 잘못 되었음 😅</p>}
+      {isLoading && <PropagateLoader />}
+      {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 존재하지 않음</p>}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
