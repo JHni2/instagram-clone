d4504fea32c91aeed61a23c6b9c6df353e364bb2
Jihyun <wlgus0260@naver.com>
2023-07-28 23:59:35 +0900

    feat: search dynamic하게 동작

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,9 @@
 import { searchUsers } from '@/service/user';
 import { NextResponse } from 'next/server';
 
+// 요청이 오면 수행하도록! (static하지 X)
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,9 @@
 import UserSearch from '@/components/UserSearch';
 import { Metadata } from 'next';
 
+// 요청이 오면 수행하도록! (static하지 X)
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: 'User Search',
   description: 'Search users to follow',
