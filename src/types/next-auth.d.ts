1386d6916bdf64812e75e66f45e5c7e17e5eeffb
Jihyun <wlgus0260@naver.com>
2023-07-28 17:55:32 +0900

    feat: 사용자 프로필 (서비스)

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,8 +1,8 @@
-import { User } from '@/model/user';
+import { AuthUser } from '@/model/user';
 
 // [...nextauth].ts에서 callbacks의 username 위한 타입 정의
 declare module 'next-auth' {
   interface Session {
-    user: User;
+    user: AuthUser;
   }
 }
