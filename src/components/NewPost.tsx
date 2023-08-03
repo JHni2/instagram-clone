6c7399e81c8509f00e94e69fb4329d9c36280369
Jihyun <wlgus0260@naver.com>
2023-08-03 17:44:13 +0900

    design: 새 포스트 작성 (UI)

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AuthUser } from '@/model/user';
+import Image from 'next/image';
 import React, { useState } from 'react';
 import PostUserAvatar from './PostUserAvatar';
 import Button from './ui/Button';
@@ -26,7 +27,7 @@ export default function NewPost({ user: { username, image } }: Props) {
   const handleDrag = (e: React.DragEvent) => {
     if (e.type === 'dragenter') {
       setDragging(true);
-    } else if (e.type === 'dreagleave') {
+    } else if (e.type === 'dragleave') {
       setDragging(false);
     }
   };
@@ -46,17 +47,27 @@ export default function NewPost({ user: { username, image } }: Props) {
   };
 
   return (
-    <section>
+    <section className="w-full max-w-xl flex flex-col items-center mt-6">
       <PostUserAvatar username={username} image={image ?? ''} />
-      <form>
+      <form className="w-full flex flex-col mt-2 gap-2">
         <input onChange={handleChange} className="hidden" type="file" name="input" id="input-upload" accept="image/*" />
-        <label htmlFor="input-upload" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDragOver} onDrop={handleDrop}>
-          <FilesIcon />
-          <p>Drag and Drop your image here or click</p>
+        <label className={`w-full h-60 flex flex-col items-center justify-center cursor-pointer ${!file && 'border-2 border-sky-500 border-dashed'}`} htmlFor="input-upload" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDragOver} onDrop={handleDrop}>
+          {dragging && <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none"></div>}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-auto">
+              <FilesIcon />
+              <p>Drag and Drop your image here or click</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image className="object-contain" src={URL.createObjectURL(file)} alt="local file" fill sizes="650px" />
+            </div>
+          )}
         </label>
-        <textarea name="text" id="input-text" required rows={10} placeholder={'Write a caption...'} />
+        <textarea className="outline-none text-lg border border-x-neutral-300" name="text" id="input-text" required rows={10} placeholder={'Write a caption...'} />
+        <Button text="Publish" onClick={() => {}} />
       </form>
-      <Button text="Publish" onClick={() => {}} />
     </section>
   );
 }
