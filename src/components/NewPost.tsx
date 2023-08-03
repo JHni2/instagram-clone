87ee7b2935f21e78f5b2b46b9530a82c50765869
Jihyun <wlgus0260@naver.com>
2023-08-03 17:57:31 +0900

    feat: 새 포스트 작성 (클라이언트)

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -2,7 +2,9 @@
 
 import { AuthUser } from '@/model/user';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useRef, useState } from 'react';
+import { PropagateLoader } from 'react-spinners';
 import PostUserAvatar from './PostUserAvatar';
 import Button from './ui/Button';
 import FilesIcon from './ui/FilesIcon';
@@ -14,6 +16,10 @@ type Props = {
 export default function NewPost({ user: { username, image } }: Props) {
   const [dragging, setDragging] = useState(false);
   const [file, setFile] = useState<File>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -46,10 +52,37 @@ export default function NewPost({ user: { username, image } }: Props) {
     }
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!file) return;
+
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', textRef.current?.value ?? '');
+
+    fetch('/api/posts', { method: 'POST', body: formData })
+      .then((res) => {
+        if (!res.ok) {
+          setError(`${res.status} ${res.statusText}`);
+          return;
+        }
+        router.push('/');
+      })
+      .catch((err) => setError(err.toString()))
+      .finally(() => setLoading(false));
+  };
+
   return (
     <section className="w-full max-w-xl flex flex-col items-center mt-6">
+      {loading && (
+        <div className="absolute inset-0 z-20 text-center pt-[20%] bg-sky-500/20">
+          <PropagateLoader />
+        </div>
+      )}
+      {error && <p className="w-full bg-red-100 text-red-600 text-center p-4 mb-4">{error}</p>}
       <PostUserAvatar username={username} image={image ?? ''} />
-      <form className="w-full flex flex-col mt-2 gap-2">
+      <form onSubmit={handleSubmit} className="w-full flex flex-col mt-2 gap-2">
         <input onChange={handleChange} className="hidden" type="file" name="input" id="input-upload" accept="image/*" />
         <label className={`w-full h-60 flex flex-col items-center justify-center cursor-pointer ${!file && 'border-2 border-sky-500 border-dashed'}`} htmlFor="input-upload" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDragOver} onDrop={handleDrop}>
           {dragging && <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none"></div>}
@@ -65,7 +98,7 @@ export default function NewPost({ user: { username, image } }: Props) {
             </div>
           )}
         </label>
-        <textarea className="outline-none text-lg border border-x-neutral-300" name="text" id="input-text" required rows={10} placeholder={'Write a caption...'} />
+        <textarea ref={textRef} className="outline-none text-lg border border-x-neutral-300" name="text" id="input-text" required rows={10} placeholder={'Write a caption...'} />
         <Button text="Publish" onClick={() => {}} />
       </form>
     </section>
